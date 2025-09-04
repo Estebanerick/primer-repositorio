501ac624972ec52699a13faba74b53eda1c7721a
Fabian <estebanerick326@gmail.com>
2025-09-04 14:25:27 -0400

    pre-menu

--- agenda.py
+++ agenda.py
@@ -1,16 +1,28 @@
-# agenda
-contacto = []
+#agenda
+contactos = []
 
 def agregar_contacto(nombre, telefono):
-    contacto.append({"nombre": nombre, "telefono:": telefono})
+    contactos.append({"nombre": nombre, "telefono:": telefono})
 
 def listar_contactos():
-    print("\nLista de contacto: ")
-    for c in contacto:
+    print("\nLista de contactos : ")
+    for c in contactos :
         print(f"{c['nombre']} - {c['telefono']}")
 
+def buscar_contacto(nombre):
+    for c in contactos:
+        if c['nombre'].lower() == nombre.lower():
+            print(f"Contacto encontrado : {c['nombre']} - {c['telefono']}")
+            return
+        print('Contacto NO encontrado')
 
-nombre = input("Ingresa el nombre: ")
-telefono = input("Ingresar el telefono: ")
-agregar_contacto(nombre, telefono)
-print("contacto agregado")
+listar_contactos()
+
+#nombre = input("Ingresa el nombre : ")
+#telefono = input("Ingresar el teléfono : ")
+#agregar_contacto(nombre, telefono)
+#print("Contacto agregado")
+
+#uso funcion buscar
+nombre_buscar = input("Nombre a buscar : ")
+buscar_contacto(nombre_buscar)
